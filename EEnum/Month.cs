2fe4a482e94583d54ca1211ed22e8dcfcb80958b
wasim <wasim@DESKTOP-IAM3DCL>
2023-02-25 13:23:07 +0200

    Add project files.

---
+++ EEnum/Month.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEnum
+{
+    enum Month
+    {
+        JAN = 1,
+        FEB,
+        MAR,
+        APR,
+        MAY,
+        JUN,
+        JUL,
+        AUG,
+        SEP,
+        OCT,
+        NOV,
+        DEC
+            
+        
+
+    }
+}
---
+++ EEnum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EEnum
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var month in Enum.GetNames(typeof(Month)))
+            {
+                Console.WriteLine($"{month} = {(int)Enum.Parse(typeof(Month),month)}" );
+            }
+            
+        }
+    }
+}
